08644819e16e3df9fd353306ff14e9d53ef4f79e
i-Senku <ercngp@gmail.com>
2022-03-19 20:26:13 +0300

    init

---
+++ iOS-Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Template
+//
+//  Created by Ercan Garip on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
