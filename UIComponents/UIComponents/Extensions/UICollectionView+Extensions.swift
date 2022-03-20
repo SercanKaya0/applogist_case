fee3541258d2c5d1ceb1950b60d980560426dae5
i-Senku <ercngp@gmail.com>
2022-03-20 11:09:43 +0300

    update frameworks

--- UIComponents/UIComponents/Extensions/UICollectionView+Extensions.swift
+++ UIComponents/UIComponents/Extensions/UICollectionView+Extensions.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import TinyConstraints
-import MobilliumBuilders
 
 public extension UICollectionView {
     
@@ -41,48 +39,4 @@ public extension UICollectionView {
         }
         return cell
     }
-    
-    func setEmptyView(message: String, image: UIImage) {
-        let emptyView = UIView()
-        emptyView.size(.init(width: bounds.width, height: bounds.height))
-        
-        let circleView = UIViewBuilder()
-            .backgroundColor(.appHoney.withAlphaComponent(0.15))
-            .cornerRadius(36)
-            .build()
-        
-        let imageView = UIImageViewBuilder()
-            .contentMode(.scaleAspectFit)
-            .image(image)
-            .build()
-        
-        let messageLabel = UILabelBuilder()
-            .numberOfLines(0)
-            .textAlignment(.center)
-            .text(message)
-            .font(.title)
-            .textColor(.appObsidian)
-            .build()
-        
-        emptyView.addSubview(circleView)
-        circleView.size(.init(width: 72, height: 72))
-        circleView.centerInSuperview()
-        
-        circleView.addSubview(imageView)
-        imageView.size(.init(width: 40, height: 40))
-        imageView.centerInSuperview()
-        
-        emptyView.addSubview(messageLabel)
-        messageLabel.topToBottom(of: circleView, offset: XMMargin)
-        messageLabel.horizontalToSuperview(insets: .horizontal(XXXLMargin))
-
-        self.backgroundView = emptyView
-    }
-    
-    func restore() {
-        self.backgroundView = nil
-    }
-    
-    // swiftlint:enable fatal_error
-    
 }
