527f0b2b1218be4e0bc79f9608bf7d63e786b84a
Sercan KAYA <sercan.kaya@icloud.com>
2022-07-21 22:00:43 +0300

    swiftgen

--- UIComponents/UIComponents/UIComponents.h
+++ UIComponents/UIComponents/UIComponents.h
@@ -5,7 +5,9 @@
 //  Created by Ercan Garip on 20.03.2022.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import <Utilities/Utilities.h>
 
 //! Project version number for UIComponents.
 FOUNDATION_EXPORT double UIComponentsVersionNumber;
