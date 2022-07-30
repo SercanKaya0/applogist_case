64739839f28fde0604745f4d9f165875a226374c
Sercan KAYA <sercan.kaya@icloud.com>
2022-07-31 02:56:33 +0300

    APLC-01 config AppDelegate

---
+++ applogist_case/AppDelegate/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  applogist_case
+//
+//  Created by Sercan Kaya on 21.10.2021.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // swiftlint:disable force_cast
+    static let shared = UIApplication.shared.delegate as! AppDelegate
+    // swiftlint:enable force_cast
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // For Facebook
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
+        let bounds = UIScreen.main.bounds
+        self.window = UIWindow(frame: bounds)
+        self.window?.makeKeyAndVisible()
+        AppRouter.shared.window = window
+        AppRouter.shared.startApp()
+        return true
+        
+    }
+    
+}
