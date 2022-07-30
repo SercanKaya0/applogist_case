b4701ee39b4260b610cc7e3826fdd894ef384da9
Sercan KAYA <sercan.kaya@icloud.com>
2022-07-31 02:55:15 +0300

    add String Extension and Loader Notification

--- Utilities/Utilities/Notification/NotificationNames.swift
+++ Utilities/Utilities/Notification/NotificationNames.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension Notification.Name {
     static let userIsLoggedIn = Notification.Name(rawValue: "userIsLoggedIn")
-    static let hideNytLoader = Notification.Name(rawValue: "hideNytLoader")
+    static let hideapplogist_caseLoader = Notification.Name(rawValue: "hideapplogist_caseLoader")
     static let scrollToTop = Notification.Name(rawValue: "scrollToTop")
     static let closeLogin = Notification.Name(rawValue: "closeLogin")
     static let screenStatusAppear = Notification.Name(rawValue: "“screenStatusAppear”")
