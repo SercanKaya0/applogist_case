3d735510973defb34c1b98174c5d7be3bd168ad9
Sercan KAYA <sercan.kaya@icloud.com>
2022-07-31 02:58:47 +0300

    APLC-01 add HomePage

---
+++ applogist_case/Scene/Home/HomeRouter.swift
@@ -0,0 +1,10 @@
+//
+//  HomeRouter.swift
+//  applogist_case
+//
+//  Created by Sercan KAYA on 29.07.2022.
+//
+
+final class HomeRouter: Router, HomeRouter.Routes {
+    typealias Routes = CheckOutRoute
+}
--- iOS-Template/Scene/Example/ExampleRouter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ExampleRouter.swift
-//  iOS-Template
-//
-//  Created by Sercan Kaya on 6.05.2022.
-//
-
-final class ExampleRouter: Router {}
