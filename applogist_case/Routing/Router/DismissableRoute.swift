8d11f0a4ee7a2fb6a6b6a9e49500e331b6e02cac
Sercan KAYA <sercan.kaya@icloud.com>
2022-07-31 02:58:07 +0300

    APLC-01 Base Config

---
+++ applogist_case/Routing/Router/DismissableRoute.swift
@@ -0,0 +1,23 @@
+//
+//  DismissableRoute.swift
+//  applogist_case
+//
+//  Created by Sercan Kaya on 30.01.2022.
+//
+
+import Foundation
+import Utilities
+
+protocol Dismissable {
+    func dismiss(isAnimated: Bool, completion: VoidClosure?)
+}
+
+extension Dismissable where Self: RouterProtocol {
+    func dismiss(isAnimated: Bool = true, completion: VoidClosure? = nil) {
+        guard let viewController = viewController else {
+            assertionFailure("Nothing to close.")
+            return
+        }
+        viewController.dismiss(animated: isAnimated, completion: completion)
+    }
+}
---
+++ applogist_case/Scene/Base/NavigationController/BaseNavigationController.swift
@@ -0,0 +1,46 @@
+//
+//  BaseNavigationController.swift
+//  applogist_case
+//
+//  Created by Sercan Kaya on 7.12.2021.
+//
+
+import UIKit
+import UIComponents
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureContents()
+        interactivePopGestureRecognizer?.delegate = nil
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func configureContents() {
+        let attributed: [NSAttributedString.Key: Any] = [ .foregroundColor: UIColor.black]
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            let backButtonAppearance = UIBarButtonItemAppearance()
+            backButtonAppearance.normal.titleTextAttributes = attributed
+            
+            appearance.backButtonAppearance = backButtonAppearance
+            appearance.backgroundColor = .white
+            appearance.titleTextAttributes = attributed
+            appearance.largeTitleTextAttributes = attributed
+
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().barTintColor = .white
+            UINavigationBar.appearance().isTranslucent = false
+        }
+
+    }
+}
